de90fe0a918d4e55639b908fbd5fe0c5bb5197b7
Isaac Powell <isaac.powell@sprbrk.com>
2019-05-27 21:48:59 -0700

    changed book streaming to a knock knock joke thing to be more interactive. promising progress in understanding this better.

--- WebSocketLearn/WebSocketLearn/Controllers/HomeController.cs
+++ WebSocketLearn/WebSocketLearn/Controllers/HomeController.cs
@@ -34,15 +34,15 @@ namespace WebSocketLearn.Controllers
             return View();
         }
 
-        [Route("dickensws")]
-        public async Task<IActionResult> DickensWs()
+        [Route("kkws")]
+        public async Task<IActionResult> JokeWs()
         {
 
             if (this.HttpContext.WebSockets.IsWebSocketRequest)
             {
 
                 WebSocket webSocket = await this.HttpContext.WebSockets.AcceptWebSocketAsync();
-                await StreamBook(this.HttpContext, webSocket);
+                await TellJokes(this.HttpContext, webSocket);
 
             }
             else
@@ -56,64 +56,101 @@ namespace WebSocketLearn.Controllers
         // have the text to send defined somewhere
         // open the text file with a stream and read amount of bytes based on the state
 
-        // have state consisting of send speed and possibly text defined
+        // have objects of knock knock jokes stored
+            // initial
+            // who punchline
+        // read jokes from file, JSON?
+        // when connection opens send Knock, knock
+        // wait for client response, if 'whose there?' send initial
+        // wait for response, if '... who?' send punchline
         // poll connection to ensure its open
-        // send apropriate amount of text data to client, according to state
+        // 
         // 
 
-        private async Task StreamBook(HttpContext context, WebSocket webSocket)
+        private async Task TellJokes(HttpContext context, WebSocket webSocket)
         {
-            var readBuffer = new byte[1024 * 4];
-            int count = 0;
 
-            string bookFilePath = @"C:\Users\ipowell\Source\Repos\web-socket-learn\WebSocketLearn\WebSocketLearn\book_text\oliver_twist.txt";
-            int countRequested = 10;
-            int readCount = 0;
-            int index = 0;
+            //var buffer = new byte[1024 * 4];
+            //WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            //while (!result.CloseStatus.HasValue)
+            //{
+            //    await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
 
-            char[] bookBuffer = await GetBookContents();
-            char[] sendBuffer = new char[10];
+            //    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            //}
+            //await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
 
-            while (count++ < 100)
+            JokeModel joke = new JokeModel("Cow says.", "No, a cow says MOOO!!");
+            byte[] readBuffer = new byte[1024 * 4];
+            ArraySegment<byte> bytesToSend = new ArraySegment<byte>();
+            string clientResponse;
+
+
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(readBuffer),
+                CancellationToken.None
+            );
+
+            // while they don't close
+            while (!result.CloseStatus.HasValue)
             {
-                // Prepare bytes to send
-                for (int i = 0; i < 10; i++)
-                {
-                    sendBuffer[i] = bookBuffer[index];
-                    index++;
-                }
+                // send knock knock
+                string kk = "Knock, knock.";
 
-                byte[] encodedText = Encoding.ASCII.GetBytes(sendBuffer);
-                ArraySegment<byte> bytesToSend = new ArraySegment<byte>(encodedText);
+                bytesToSend = StringToBytes(kk);
 
                 await webSocket.SendAsync(
                     bytesToSend,
-                    WebSocketMessageType.Text,
-                    true,
+                    result.MessageType,
+                    result.EndOfMessage,
                     CancellationToken.None
                 );
-            }
 
-            await webSocket.SendAsync(
-                new ArraySegment<byte>(null),
-                WebSocketMessageType.Binary,
-                false,
-                CancellationToken.None
-            );
+                // await whose there
+                result = await webSocket.ReceiveAsync(
+                    new ArraySegment<byte>(readBuffer),
+                    CancellationToken.None
+                );
+
+                // send setup
+                clientResponse = Encoding.ASCII.GetString(readBuffer);
+
+                // client response is being padded. need to strip that out and then we are very close
+
+                if (clientResponse == "Who's there?")
+                {
+                    bytesToSend = StringToBytes(joke.Who);
+
+                    await webSocket.SendAsync(
+                        bytesToSend,
+                        result.MessageType,
+                        result.EndOfMessage,
+                        CancellationToken.None
+                    );
+                }
+                //send punch line
+                else if (clientResponse == "... who?")
+                {
+                    bytesToSend = StringToBytes(joke.Punchline);
+
+                    await webSocket.SendAsync(
+                        bytesToSend,
+                        result.MessageType,
+                        result.EndOfMessage,
+                        CancellationToken.None
+                    );
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
 
-        private async Task<char[]> GetBookContents()
+        public ArraySegment<byte> StringToBytes(string s)
         {
-            string bookFilePath = @"C:\Users\ipowell\Source\Repos\web-socket-learn\WebSocketLearn\WebSocketLearn\book_text\oliver_twist.txt";
-            char[] buffer;
-
-            using (StreamReader sr = new StreamReader(bookFilePath))
-            {
-                buffer = new char[sr.BaseStream.Length];
-                await sr.ReadAsync(buffer, 0, (int)sr.BaseStream.Length);
-            }
-
-            return buffer;
+            byte[] encodedText = Encoding.ASCII.GetBytes(s);
+            return new ArraySegment<byte>(encodedText);
         }
 
         public IActionResult Privacy()
---
+++ WebSocketLearn/WebSocketLearn/Models/JokeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSocketLearn.Models
+{
+    public class JokeModel
+    {
+        public string Who { get; set; }
+        public string Punchline { get; set; }
+
+        public JokeModel(string who, string punchline)
+        {
+            Who = who;
+            Punchline = punchline;
+        }
+    }
+}
